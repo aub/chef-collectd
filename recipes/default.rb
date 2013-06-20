3d97267bdae6506f056c4ffe699ec277901f3ea9
Aubrey Holland <aubreyholland@gmail.com>
2013-06-19 23:13:29 -0400

    change the way the source gets installed

--- recipes/default.rb
+++ recipes/default.rb
@@ -63,21 +63,29 @@ if node["collectd"]["plugins"]
   end
 end
 
+src_filepath = "#{Chef::Config[:file_cache_path]}/collectd-#{node["collectd"]["version"]}.tar.gz"
+
+remote_file node['collectd']['url'] do
+  source node["collectd"]["url"]
+  checksum node["collectd"]["checksum"]
+  path src_filepath
+  # notifies :run, "bash[install-collectd]", :immediately
+  # action :create_if_missing
+  backup false
+end
+
 bash "install-collectd" do
-  cwd Chef::Config[:file_cache_path]
+  cwd ::File.dirname(src_filepath)
   code <<-EOH
     tar -xzf collectd-#{node["collectd"]["version"]}.tar.gz
     (cd collectd-#{node["collectd"]["version"]} && ./configure --prefix=#{node["collectd"]["dir"]} && make && make install)
   EOH
-  action :nothing
-  not_if "#{node["collectd"]["dir"]}/sbin/collectd -h 2>&1 | grep #{node["collectd"]["version"]}"
-end
-
-remote_file "#{Chef::Config[:file_cache_path]}/collectd-#{node["collectd"]["version"]}.tar.gz" do
-  source node["collectd"]["url"]
-  checksum node["collectd"]["checksum"]
-  notifies :run, "bash[install-collectd]", :immediately
-  action :create_if_missing
+  not_if do
+    binary = "#{node["collectd"]["dir"]}/sbin/collectd"
+    ::File.exists?(binary) &&
+      (`#{binary} -v` || '').include?(node["collectd"]["version"])
+  end
+  notifies :restart, "service[collectd]"
 end
 
 template "/etc/init.d/collectd" do
